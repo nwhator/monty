78ae0722a56d30a17c3eca1ab88a19b98085a68d
Itsjustdice <ckdebruin@gmail.com>
2023-06-22 09:46:58 -0700

    updates

--- main.c
+++ main.c
@@ -8,38 +8,38 @@ bus_t interpret = {NULL, NULL, NULL, 0};
 */
 int main(int argc, char *argv[])
 {
-	char *content;
-	FILE *new_file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *new_stack = NULL;
-	unsigned int line = 0;
+	char *content; /* Variable to store content of each line read from file */
+	FILE *new_file; /* Pointer to file to be opened */
+	size_t size = 0; /* set size for getline() function */
+	ssize_t read_line = 1; /* Number of characters read by getline() function */
+	stack_t *new_stack = NULL; /* Pointer to stack structure */
+	unsigned int line = 0; /* Counter to keep track of the line number */
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE); /* rid program with a failure status */
 	}
 	new_file = fopen(argv[1], "r");
 	interpret.file = new_file;
 	if (!new_file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE); /* rid program with a failure status */
 	}
 	while (read_line > 0)
 	{
-		content = NULL;
+		content = NULL; /* Reset content pointer for each line read */
 		read_line = getline(&content, &size, new_file);
 		interpret.content = content;
-		line++;
+		line++; /* Increment the line number counter */
 		if (read_line > 0)
 		{
 			executeMonty(content, &new_stack, line, new_file);
 		}
-		free(content);
+		free(content); /* Free memory allocated by getline() for content of line */
 	}
-	free_list(new_stack);
-	fclose(new_file);
-return (0);
+	free_list(new_stack); /* Free memory allocated for stack structure */
+	fclose(new_file); /* Close the file */
+return (0); /* Return 0 to indicate successful program execution */
 }
--- mod_op.c
+++ mod_op.c
@@ -1,32 +1,30 @@
 #include "monty.h"
-
 /**
  * mod_op - Computes the remainder of division for top stack elements
  * top element of the stack by the top element of the stack
  * @top: Pointer to the stack
  * @line: Current line number
 */
-
 void mod_op(stack_t **top, unsigned int line)
 {
-	stack_t *current;
+	stack_t *current; /* Pointer to traverse the stack */
 	int len = 0, sum;
 
-	current = *top;
+	current = *top; /* Set 'current' to point to top of stack */
 	while (current)
 	{
-		current = current->next;
-		len++;
+		current = current->next; /* move 'current' to the next node in the stack */
+		len++; /* Increment the length counter */
 	}
 	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't mod, stack too short\n", line);
-		fclose(interpret.file);
-		free(interpret.content);
-		free_list(*top);
-		exit(EXIT_FAILURE);
+		fclose(interpret.file); /* Close the file */
+		free(interpret.content); /* Free memory allocated for line content */
+		free_list(*top); /* free memory allocated for stack */
+		exit(EXIT_FAILURE); /* rid program with a failure status */
 	}
-	current = *top;
+	current = *top; /* Reset 'current' to point to top of stack */
 	if (current->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line);
@@ -36,7 +34,7 @@ void mod_op(stack_t **top, unsigned int line)
 		exit(EXIT_FAILURE);
 	}
 	sum = current->next->n % current->n;
-	current->next->n = sum;
-	*top = current->next;
-	free(current);
+	current->next->n = sum; /* Store result of modulo operation in 2nd element */
+	*top = current->next; /* Update top of stack to 2nd element */
+	free(current); /* Free memory allocated for prev top element */
 }
