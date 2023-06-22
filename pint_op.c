4150d689d495c99cef63b9d435cc36e1be64c06f
Itsjustdice <ckdebruin@gmail.com>
2023-06-22 11:07:55 -0700

    updates

--- pint_op.c
+++ pint_op.c
@@ -4,16 +4,15 @@
  * @top: Pointer to the stack
  * @line: Line number of the command
 */
-
 void pint_op(stack_t **top, unsigned int line)
 {
 	if (*top == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line);
-		fclose(interpret.file);
-		free(interpret.content);
-		free_list(*top);
-		exit(EXIT_FAILURE);
-	}
+		fclose(interpret.file); /* Close the file */
+		free(interpret.content); /* Free memory allocated for line content */
+		free_list(*top); /* Free the memory allocated for the stack */
+		exit(EXIT_FAILURE); /* rid program with a failure */
+	} /* Print value (integer) stored in top node of stack */
 	printf("%d\n", (*top)->n);
 }
--- pop_op.c
+++ pop_op.c
@@ -6,17 +6,17 @@
 */
 void pop_op(stack_t **top, unsigned int line)
 {
-	stack_t *current;
+	stack_t *current; /* Pointer to traverse the stack */
 
 	if (*top == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line);
-		fclose(interpret.file);
-		free(interpret.content);
-		free_list(*top);
-		exit(EXIT_FAILURE);
+		fclose(interpret.file); /* Close the file */
+		free(interpret.content); /* Free memory allocated for line content */
+		free_list(*top); /* Free memory allocated for stack */
+		exit(EXIT_FAILURE); /* rid program with a failure status */
 	}
-	current = *top;
-	*top = current->next;
-	free(current);
+	current = *top; /* Set 'current' to point to top of stack */
+	*top = current->next; /* Update top of stack to next node */
+	free(current); /* Free memory allocated for prev top node */
 }
