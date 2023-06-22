139302bd304c7cb77cdc65a4d3f648304d8dfccb
Itsjustdice <ckdebruin@gmail.com>
2023-06-22 10:50:12 -0700

    updates

--- pall_op.c
+++ pall_op.c
@@ -1,22 +1,20 @@
 #include "monty.h"
-
 /**
  * pall_op - Prints the elements of the stack
  * @top: Pointer to the stack
  * @line: Line number in the script (unused)
 */
-
 void pall_op(stack_t **top, unsigned int line)
 {
-	stack_t *current;
-	(void)line;
+	stack_t *current; /* Pointer to traverse stack */
+	(void)line; /* Silence compiler warning about an unused variable 'line' */
 
-	current = *top;
+	current = *top; /* Set 'current' to point to the top of stack */
 	if (current == NULL)
-		return;
+		return; /* If stack is empty, return without performing further actions */
 	while (current)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
+		printf("%d\n", current->n); /* Print value of current node in stack */
+		current = current->next; /* move 'current' to the next node in the stack */
 	}
 }
--- pchar_op.c
+++ pchar_op.c
@@ -1,23 +1,21 @@
 #include "monty.h"
-
 /**
  * pchar_op - Prints the char at the top of the stack, followed by a new line
  * @top: Pointer to the Pointer to the top of the stack
  * @line: Line number in the script
 */
-
 void pchar_op(stack_t **top, unsigned int line)
 {
-	stack_t *current;
+	stack_t *current; /* Pointer to traverse stack */
 
-	current = *top;
+	current = *top; /* Set 'current' to point to top of stack */
 	if (!current)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line);
-		fclose(interpret.file);
-		free(interpret.content);
-		free_list(*top);
-		exit(EXIT_FAILURE);
+		fclose(interpret.file); /* Close the file */
+		free(interpret.content); /* free memory allocated for line content */
+		free_list(*top); /* Free memory allocated for stack */
+		exit(EXIT_FAILURE); /* rid program with a failure status */
 	}
 	if (current->n > 127 || current->n < 0)
 	{
@@ -26,6 +24,6 @@ void pchar_op(stack_t **top, unsigned int line)
 		free(interpret.content);
 		free_list(*top);
 		exit(EXIT_FAILURE);
-	}
+	} /* Print character corresponding to value in top node of stack */
 	printf("%c\n", current->n);
 }
