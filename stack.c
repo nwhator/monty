b45707720fb92a0ae81a3107a2462797eaa98f63
Itsjustdice <ckdebruin@gmail.com>
2023-06-22 12:40:53 -0700

    updates

--- stack.c
+++ stack.c
@@ -6,7 +6,10 @@
 */
 void stackTop(stack_t **top, unsigned int line)
 {
+	/* line is used to suppress "unused variable" warning for 'top' parameter */
 	(void)top;
+	/* line is used to suppress  "unused variable" warning for 'line' parameter */
 	(void)line;
+	/* Set 'lifi' field of 'interpret' struct to 0 */
 	interpret.lifi = 0;
 }
